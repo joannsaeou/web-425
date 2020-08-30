fe00eb6644c8e99192904e2ffe02a50c22cf857b
joann saeou <joannssaeou@Gmail.com>
2020-08-30 15:58:47 -0700

    Update book-list.component.spec.ts

--- week-5/in-n-out-books3/src/app/book-list/book-list.component.spec.ts
+++ week-5/in-n-out-books3/src/app/book-list/book-list.component.spec.ts
@@ -1,3 +1,11 @@
+/**
+ * Title:book-list.component.spec.ts
+ * Author: Joann Saeou
+ * Date: 30 August 2020
+ * Description:fixing code attribution
+ */
+
+
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BookListComponent } from './book-list.component';
