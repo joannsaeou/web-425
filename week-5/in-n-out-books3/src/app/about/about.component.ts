e17d9b5b8785cd68ef15b72ce4b6ff562f67b6ba
joann saeou <joannssaeou@Gmail.com>
2020-08-30 15:55:56 -0700

    Update about.component.ts

--- week-5/in-n-out-books3/src/app/about/about.component.ts
+++ week-5/in-n-out-books3/src/app/about/about.component.ts
@@ -1,3 +1,11 @@
+/**
+ * Title:about.component.ts
+ * Author: Joann Saeou
+ * Date: 30 August 2020
+ * Description:fixing code attribution
+ */
+
+
 import { Component, OnInit } from '@angular/core';
 
 @Component({
