650ff4bcef76acd3fd1b7ae98d4210da581cc050
joann saeou <joannssaeou@Gmail.com>
2020-08-30 16:07:06 -0700

    Update not-found.component.ts

--- week-6/gpa-calculator-app1/src/app/not-found/not-found.component.ts
+++ week-6/gpa-calculator-app1/src/app/not-found/not-found.component.ts
@@ -1,3 +1,10 @@
+/**
+ * Title:not-found.component.ts
+ * Author: Joann Saeou
+ * Date: 30 August 2020
+ * Description:fixing code attribution
+ */
+
 import { Component, OnInit } from '@angular/core';
 
 @Component({
