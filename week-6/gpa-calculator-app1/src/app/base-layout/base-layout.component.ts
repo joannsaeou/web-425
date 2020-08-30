1f9469bd47efa1d0605304521a5f6ec223ba3c5c
joann saeou <joannssaeou@Gmail.com>
2020-08-30 16:02:59 -0700

    Update base-layout.component.ts

--- week-6/gpa-calculator-app1/src/app/base-layout/base-layout.component.ts
+++ week-6/gpa-calculator-app1/src/app/base-layout/base-layout.component.ts
@@ -1,3 +1,11 @@
+/**
+ * Title:base-layout.component.ts
+ * Author: Joann Saeou
+ * Date: 30 August 2020
+ * Description:fixing code attribution
+ */
+
+
 import { Component, OnInit } from '@angular/core';
 
 @Component({
