d61a1fedb56ac7ebe8a2a62527ff8a7cd9d0ec97
joann saeou <joannssaeou@Gmail.com>
2020-08-30 16:04:32 -0700

    Update home.component.ts

--- week-6/gpa-calculator-app1/src/app/home/home.component.ts
+++ week-6/gpa-calculator-app1/src/app/home/home.component.ts
@@ -1,3 +1,11 @@
+/**
+ * Title:home.component.ts
+ * Author: Joann Saeou
+ * Date: 30 August 2020
+ * Description:fixing code attribution
+ */
+
+
 import { Component, OnInit } from '@angular/core';
 
 @Component({
